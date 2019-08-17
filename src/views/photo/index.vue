810ae652b12d06610a64d18711a19a7134d4bf94
evanliu2968 <evanliu2968@gmail.com>
2019-08-17 15:01:28 +0800

    增加photo-image-viewer

--- src/views/photo/index.vue
+++ src/views/photo/index.vue
@@ -3,7 +3,7 @@
     <div class="content">
       <div :class="['waterfall', `column-count-2`]">
         <div v-for="(item, i) in photos" :key="i" class="waterfall-item">
-          <div class="img-box" :title="item.title">
+          <div class="img-box" :title="item.title" @click="() => toggleCarousel(i)">
             <el-image :src="item.img" lazy style="width: 100%">
               <div slot="placeholder" class="flex-center-row image-slot" style="min-height: 320px">
                 <img class="loading" src="/public/images/loading.gif">
@@ -20,28 +20,49 @@
         </div>
       </div>
     </div>
+    <el-image-viewer
+      ref="imageViewer"
+      v-if="showViewer"
+      :on-close="closeViewer"
+      :url-list="photos.map(v => v.img)"
+    ></el-image-viewer>
   </MainContainer>
 </template>
 <script>
 import MainContainer from '@/components/layout/Container'
+import ElImageViewer from './image-viewer'
+
 import { getCloverPhotos } from '@/api'
 import './index.less'
+
 export default {
   components: {
+    ElImageViewer,
     MainContainer
   },
   data() {
     return {
-      photos: []
+      photos: [],
+      showViewer: false
     }
   },
   mounted() {
     getCloverPhotos().then(res => {
       console.log(res)
       this.photos = res.photoList
+      this.photoUrls = this.photos.map(v => v.img)
     })
   },
   methods: {
+    toggleCarousel(i) {
+      this.showViewer = true
+      this.$nextTick(() => {
+        this.$refs.imageViewer.setActiveIndex(i)
+      })
+    },
+    closeViewer() {
+      this.showViewer = false
+    }
   }
 }
 </script>
